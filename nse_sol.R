9ecff267c38f5e007a9d2735c00555d3ac77c9ff
Andreas Hofheinz <andreas.hofheinz@outlook.com>
2020-02-28 14:12:43 +0100

    Finished Exercise 2

--- nse_sol.R
+++ nse_sol.R
@@ -2,21 +2,31 @@
 get2 <- function(x, envir) {
   checkmate::assert_character(x, len = 1, all.missing = FALSE, null.ok = FALSE)
   checkmate::assert_environment(envir)
-  
+
   object_name <- as.name(x)
   # Try-catch is only used in this example to demonstrate the possibility with the envnames package
   # for "later me" :-)
-  object = tryCatch({
+  object <- tryCatch({
     eval(object_name, envir = envir)
   }, error = function(err) {
     # By using envnames package we could give a more detailed error message
-    #envname <- envnames::environment_name(envir)
-    #err_msg <- paste0("Object `", x, "` could not be found in environment `", envname, "`")
-    #err$message <- err_msg
+    # envname <- envnames::environment_name(envir)
+    # err_msg <- paste0(
+    #   "Variable `", deparse(substitute(x, env = rlang::env_parent())),
+    #   "` could not be found in environment `", envname, "`"
+    # )
+    # err$message <- err_msg
     stop(err)
-
   })
-  
   object
+}
+
+
+
+
+assign2 <- function(x, value, env) {
+  checkmate::assert_character(x, len = 1, all.missing = FALSE, null.ok = FALSE)
+  checkmate::assert_environment(env)
+  eval(substitute(x <- value, list(x = as.name(x), value = value)), envir  = env)
 }
 
--- tests-nse-sol.R
+++ tests-nse-sol.R
@@ -12,3 +12,23 @@ test_that("get2 works correctly", {
 })
 
 
+context("Function assign2")
+
+test_that("assign2 works correctly", {
+  test_env <- new.env()
+  test_env$test_var <- 2
+  assign2("test_var", 3, test_env)
+  expect_equal(3, test_env$test_var)
+  assign2("test_var", "x", test_env)
+  expect_equal("x", test_env$test_var)
+  my_new_func <- function(t, value, env) {
+    x <- 10
+    assign2(t, value, env)
+  }
+  my_new_func("test_var", 8, test_env)
+  expect_equal(8, test_env$test_var)
+  expect_error(assign2("test_var", 3, "not an env"), "not 'character'")
+  expect_error(assign2(mtcars, 3, globalenv()), "not 'data.frame'")
+  assign2("test_var2", "x", test_env)
+  expect_equal("x", test_env$test_var2)
+})
